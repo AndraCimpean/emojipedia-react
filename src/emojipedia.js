daea43fc3a921742d1d31da2e269456b4012f16d
AndraCimpean <andra.cimpean94@gmail.com>
2023-03-13 23:33:31 +0200

    add project files

---
+++ src/emojipedia.js
@@ -0,0 +1,90 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🙂",
+    name: "Smiling Emoji",
+    meaning:
+      "This is the type of emoji you might get when you tell your jealous coworker about your new promotion."
+  },
+
+  {
+    id: 5,
+    emoji: "😆",
+    name: "Laughing",
+    meaning:
+      "The emoji depicts laughter often used to react towards something hilarious"
+  },
+
+  {
+    id: 6,
+    emoji: "😑",
+    name: "Expressionless",
+    meaning:
+      "The flat eyes and mouth convey a sense of frustration or disappointment"
+  },
+
+  {
+    id: 7,
+    emoji: "😵",
+    name: "Face with crossed-out eyes",
+    meaning: "This emoji denotes disbelief or in some cases even death"
+  },
+  {
+    id: 8,
+    emoji: "🥸",
+    name: "Disguised face",
+    meaning: "Sneaking into solving a particular case or matter"
+  },
+
+  {
+    id: 9,
+    emoji: "😯",
+    name: "Hushed face",
+    meaning: "The emoji depicts mild amusement or surprise"
+  },
+
+  {
+    id: 10,
+    emoji: "😧",
+    name: "Anguished face",
+    meaning: "Depicting sadness and disappointment at the same time"
+  },
+
+  {
+    id: 11,
+    emoji: "🌐",
+    name: "Globe with meridians",
+    meaning: "This emoji is generally associated with the internet"
+  },
+
+  {
+    id: 12,
+    emoji: "🧱",
+    name: "Briks",
+    meaning:
+      "An emoji of bricks that are used in building homes, schools and tons of other structures for an individual or the public"
+  }
+];
+
+export default emojipedia;
